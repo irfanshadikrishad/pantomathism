8e20da1d0b999f0c48187de55cd7689a17493eeb
irfanshadikrishad <irfanshadikofficial@gmail.com>
2023-07-28 23:38:53 +0600

    registration salted

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { hash } = require('bcrypt');
 const router = express.Router();
 const User = require('../models/user');
+
+const SALT = Number(process.env.SALT);
 
 router.post('/registration', async (req, res) => {
     const { name, email, password } = await req.body;
@@ -8,13 +11,22 @@ router.post('/registration', async (req, res) => {
         if (data) {
             res.status(400).json({ message: "User already exists" });
         } else {
-            const user = new User({
-                name, email, password
-            });
-            user.save().then(data => {
-                res.status(201).json({ message: "Registration Successfull!" })
-            }).catch(err => {
-                res.status(400).json({ message: `Registration Save failed ${err}` })
+            hash(password, SALT, function (error, hash) {
+                if (error) {
+                    console.log(`[!ok] ${error}`);
+                    res.status(400).json({ message: "Salting Error" });
+                } else {
+                    const user = new User({
+                        name: name,
+                        email: email,
+                        password: hash
+                    });
+                    user.save().then(data => {
+                        res.status(201).json({ message: "Registration Successfull!" })
+                    }).catch(err => {
+                        res.status(400).json({ message: `Registration Save failed ${err}` })
+                    })
+                }
             })
         }
     })
